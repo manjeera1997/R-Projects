55c01686424131a7e2b289320ad0d06974298130
Naga Manjeera <manjeera.008@gmail.com>
2021-06-17 18:37:48 +0530

    Add files via upload

---
+++ model selection.R
@@ -0,0 +1,41 @@
+data <- diabetes
+head(data)
+library(VGAM)
+
+#Logit Function
+
+model1 <- glm(Outcome~Pregnancies+Glucose+BloodPressure+SkinThickness+Insulin+BMI+DiabetesPedigreeFunction+Age, data = data, 
+              family=binomial("logit"))
+summary(model1)
+anova(model1)
+
+#Multinomial Logistic Regression Model
+
+model2 <- vglm(Outcome~Pregnancies+Glucose+BloodPressure+SkinThickness+Insulin+BMI+DiabetesPedigreeFunction+Age, data = data, 
+               family=multinomial)
+summary(model2)
+
+#Poisson Regression Model
+
+model3 <- glm(Outcome~Pregnancies+Glucose+BloodPressure+SkinThickness+Insulin+BMI+DiabetesPedigreeFunction+Age, data = data, 
+              family=poisson)
+summary(model3)
+anova(model3)
+
+#For Model Selection in R we will compare AIC and BIC values of models.
+
+#AIC values for model1, model2 and model3
+
+AIC(model1)
+AIC(model2)
+AIC(model3)
+
+# We observe that AIC values for the both model1 and model2 are low when compared with model3. We can also observe that AIC values for both model and model2 are equal.
+
+#BIC values for model1, model2 and model3
+
+BIC(model1)
+BIC(model2)
+BIC(model3)
+
+#We can see that BIC values for model1 and model2 are less when compared with model3. We can also observe that BIC values for both model and model2 are equal.
